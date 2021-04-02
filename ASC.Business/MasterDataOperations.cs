75f0e2bf3ca1246e24eb987cfd040ad71b93bc5a
Rafa C <rafasantcont@yahoo.com.br>
2021-04-02 19:37:26 -0300

    Managing Master Data in Azure Tables

---
+++ ASC.Business/MasterDataOperations.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using ASC.Business.Interfaces;
+using ASC.Models.Models;
+using ASC.DataAccess.Interfaces;
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+
+namespace ASC.Business
+{
+    public class MasterDataOperations : IMasterDataOperations
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+        public MasterDataOperations(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<List<MasterDataKey>> GetAllMasterKeysAsync()
+        {
+            var masterKeys = await _unitOfWork.Repository<MasterDataKey>().FindAllAsync();
+            
+            return masterKeys.ToList();
+        }
+
+        public async Task<List<MasterDataValue>> GetAllMasterValuesAsync()
+        {
+            var masterValues = await _unitOfWork.Repository<MasterDataValue>().FindAllAsync();
+
+            return masterValues.ToList();
+        }
+
+        public async Task<List<MasterDataValue>> GetAllMasterValuesByKeyAsync(string key)
+        {
+            var masterKeys = await _unitOfWork.Repository<MasterDataValue>().FindAllByPartitionKeyAsync(key);
+            
+            return masterKeys.ToList();
+        }
+
+        public async Task<List<MasterDataKey>> GetMaserKeyByNameAsync(string name)
+        {
+            var masterKeys = await _unitOfWork.Repository<MasterDataKey>().FindAllByPartitionKeyAsync(name);
+            
+            return masterKeys.ToList();
+        }
+
+        public async Task<MasterDataValue> GetMasterValueByNameAsync(string key, string name)
+        {
+            var masterValues = await _unitOfWork.Repository<MasterDataValue>().FindAsync(key, name);
+            
+            return masterValues;
+        }
+
+        public async Task<bool> InsertMasterKeyAsync(MasterDataKey key)
+        {
+            using (_unitOfWork)
+            {
+                await _unitOfWork.Repository<MasterDataKey>().AddAsync(key);
+                _unitOfWork.CommitTransaction();
+                
+                return true;
+            }
+        }
+
+        public async Task<bool> InsertMasterValueAsync(MasterDataValue value)
+        {
+            using (_unitOfWork)
+            {
+                await _unitOfWork.Repository<MasterDataValue>().AddAsync(value);
+                _unitOfWork.CommitTransaction();
+                
+                return true;
+            }
+        }
+
+        public async Task<bool> UpdateMasterKeyAsync(string orginalPartitionKey, MasterDataKey key)
+        {
+            using (_unitOfWork)
+            {
+                var masterKey = await _unitOfWork.Repository<MasterDataKey>().FindAsync(orginalPartitionKey, key.RowKey);
+                masterKey.IsActive = key.IsActive;
+                masterKey.IsDeleted = key.IsDeleted;
+                masterKey.Name = key.Name;
+                await _unitOfWork.Repository<MasterDataKey>().UpdateAsync(masterKey);
+                _unitOfWork.CommitTransaction();
+                
+                return true;
+            }
+        }
+
+        public async Task<bool> UpdateMasterValueAsync(string originalPartitionKey, string originalRowKey, MasterDataValue value)
+        {
+            using (_unitOfWork)
+            {
+                var masterValue = await _unitOfWork.Repository<MasterDataValue>().
+                FindAsync(originalPartitionKey, originalRowKey);
+                masterValue.IsActive = value.IsActive;
+                masterValue.IsDeleted = value.IsDeleted;
+                masterValue.Name = value.Name;
+                await _unitOfWork.Repository<MasterDataValue>().UpdateAsync(masterValue);
+                _unitOfWork.CommitTransaction();
+                
+                return true;
+            }
+        }
+    }
+}
---
+++ ASC.Web/Controllers/MasterDataController.cs
@@ -0,0 +1,114 @@
+﻿using ASC.Business.Interfaces;
+using ASC.Models.Models;
+using ASC.Utilities;
+using ASC.Web.Models.MasterDataViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASC.Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class MasterDataController : BaseController
+    {
+        private readonly IMasterDataOperations _masterData;
+        private readonly IMapper _mapper;
+
+        public MasterDataController(IMasterDataOperations masterData, IMapper mapper)
+        {
+            _masterData = masterData;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MasterKeys()
+        {
+            var masterKeys = await _masterData.GetAllMasterKeysAsync();
+            var masterKeysViewModel = _mapper.Map < List<MasterDataKey>, List<MasterDataKeyViewModel>> (masterKeys);
+            // Hold all Master Keys in session
+            HttpContext.Session.SetSession("MasterKeys", masterKeysViewModel);
+            
+            return View(new MasterKeysViewModel
+            {
+                MasterKeys = masterKeysViewModel == null ? null : masterKeysViewModel.ToList(),
+                IsEdit = false
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MasterKeys(MasterKeysViewModel model)
+        {
+            model.MasterKeys = HttpContext.Session.GetSession<List<MasterDataKeyViewModel>>("MasterKeys");
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var masterKey = _mapper.Map<MasterDataKeyViewModel, MasterDataKey>(model.MasterKeyInContext);
+            if (model.IsEdit)
+            {
+                // Update Master Key
+                await _masterData.UpdateMasterKeyAsync(model.MasterKeyInContext.
+                PartitionKey, masterKey);
+            }
+            else
+            {
+                // Insert Master Key
+                masterKey.RowKey = Guid.NewGuid().ToString();
+                masterKey.PartitionKey = masterKey.Name;
+                await _masterData.InsertMasterKeyAsync(masterKey);
+            }
+
+            return RedirectToAction("MasterKeys");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MasterValues()
+        {
+            // Get All Master Keys and hold them in ViewBag for Select tag
+            ViewBag.MasterKeys = await _masterData.GetAllMasterKeysAsync();
+            return View(new MasterValuesViewModel
+            {
+                MasterValues = new List<MasterDataValueViewModel>(),
+                IsEdit = false
+            });
+        }
+        [HttpGet]
+        public async Task<IActionResult> MasterValuesByKey(string key)
+        {
+            // Get Master values based on master key.
+            return Json(new { data = await _masterData.GetAllMasterValuesByKeyAsync(key) });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MasterValues(bool isEdit, MasterDataValueViewModel masterValue)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json("Error");
+            }
+            var masterDataValue = _mapper.Map<MasterDataValueViewModel,
+            MasterDataValue>(masterValue);
+            if (isEdit)
+            {
+                // Update Master Value
+                await _masterData.UpdateMasterValueAsync(masterDataValue.PartitionKey,
+                masterDataValue.RowKey, masterDataValue);
+            }
+            else
+            {
+                // Insert Master Value
+                masterDataValue.RowKey = Guid.NewGuid().ToString();
+                await _masterData.InsertMasterValueAsync(masterDataValue);
+            }
+            return Json(true);
+        }
+    }
+}
