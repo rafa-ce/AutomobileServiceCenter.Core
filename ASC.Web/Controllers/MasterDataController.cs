286775b4383e7466913bb2c8758126e696ac2a77
Rafa C <rafasantcont@yahoo.com.br>
2021-04-02 23:55:54 -0300

    Importing Master Data to Azure Tables from Excel

--- ASC.Web/Controllers/MasterDataController.cs
+++ ASC.Web/Controllers/MasterDataController.cs
@@ -6,8 +6,10 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -109,6 +111,57 @@ namespace ASC.Web.Controllers
                 await _masterData.InsertMasterValueAsync(masterDataValue);
             }
             return Json(true);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UploadExcel()
+        {
+            var files = Request.Form.Files;
+            // Validations
+            if (!files.Any())
+            {
+                return Json(new { Error = true, Text = "Upload a file" });
+            }
+            var excelFile = files.First();
+            if (excelFile.Length <= 0)
+            {
+                return Json(new { Error = true, Text = "Upload a file" });
+            }
+            // Parse Excel Data
+            var masterData = await ParseMasterDataExcel(excelFile);
+            var result = await _masterData.UploadBulkMasterData(masterData);
+            return Json(new { Success = result });
+        }
+
+        private async Task<List<MasterDataValue>> ParseMasterDataExcel(IFormFile excelFile)
+        {
+            var masterValueList = new List<MasterDataValue>();
+            using (var memoryStream = new MemoryStream())
+            {
+                // Get MemoryStream from Excel file
+                await excelFile.CopyToAsync(memoryStream);
+                // Create a ExcelPackage object from MemoryStream
+                using (ExcelPackage package = new ExcelPackage(memoryStream))
+                {
+                    // Get the first Excel sheet from the Workbook
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.First();
+                    int rowCount = worksheet.Dimension.Rows;
+                    // Iterate all the rows and create the list of MasterDataValue
+                    // Ignore first row as it is header
+                    for (int row = 2; row <= rowCount; row++)
+                    {
+                        var masterDataValue = new MasterDataValue();
+                        masterDataValue.RowKey = Guid.NewGuid().ToString();
+                        masterDataValue.PartitionKey = worksheet.Cells[row, 1].Value.ToString();
+                        masterDataValue.Name = worksheet.Cells[row, 2].Value.ToString();
+                        masterDataValue.IsActive = Boolean.Parse(worksheet.Cells[row, 3].
+                        Value.ToString());
+                        masterValueList.Add(masterDataValue);
+                    }
+                }
+            }
+            return masterValueList;
         }
     }
 }
